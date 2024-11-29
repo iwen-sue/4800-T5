5a57c0ac3d6b06a75f807f03f4098de3c8a00221
iwen-sue <grace2268@hotmail.com>
2024-11-28 21:49:14 -0800

    fix

--- config/sns.js
+++ config/sns.js
@@ -2,7 +2,7 @@ const AWS = require('aws-sdk');
 
 const sns = new AWS.SNS({
     region: 'us-west-2',
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    accessKeyId: process.env.AWS_ACCESS_KEY,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
     });
 
